3751926454148d3cea4f8f54a69530e53be35536
vburlak <vburlak.ru@gmail.com>
2012-11-29 04:27:01 +0400

    Admin menus removing
    removing admin menu items

--- functions.php
+++ functions.php
@@ -42,6 +42,30 @@
     	));
     }
     
+	function remove_menus () {
+	global $menu;
+		$restricted = array(
+							__('Dashboard'), 
+							__('Posts'), 
+							__('Media'), 
+							__('Links'), 
+							__('Pages'), 
+							__('Appearance'), 
+							__('Tools'), 
+							__('Users'), 
+							__('Settings'), 
+							__('Comments'), 
+							__('Plugins')
+						);
+		end ($menu);
+		while (prev($menu)){
+			$value = explode(' ',$menu[key($menu)][0]);
+			if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+		}
+	}
+//	add_action('admin_menu', 'remove_menus');
+    
+    
     /* Disable WordPress Admin Bar for all users but admins. */
     show_admin_bar(false);
     

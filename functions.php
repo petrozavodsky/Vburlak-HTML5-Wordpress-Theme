1ac1964a8040cb5097c17e471324eb79d261a196
vburlak <vburlak.ru@gmail.com>
2013-02-28 14:21:27 +0400

    New Jquery in functions.php

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@
 		function core_mods() {
 			if ( !is_admin() ) {
 				wp_deregister_script('jquery');
-				wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"), false);
+				wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"), false);
 				wp_enqueue_script('jquery');
 			}
 		}

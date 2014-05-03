265d216eabae8bb4f68b1ca828df5f20eb62438e
vovasik <petrozavodsky@gmail.com>
2014-05-03 12:44:40 +0400

    1 - Правильное подключение скриптов

--- functions.php
+++ functions.php
@@ -1,52 +1,65 @@
 <?php
-        // Translations can be filed in the /languages/ directory
-        load_theme_textdomain( 'vburlakFramework', TEMPLATEPATH . '/languages' );
- 
-        $locale = get_locale();
-        $locale_file = TEMPLATEPATH . "/languages/$locale.php";
-        if ( is_readable($locale_file) )
-            require_once($locale_file);
-	
-	// Add RSS links to <head> section
-	automatic_feed_links();
-	
-	// Load jQuery
-	if ( !function_exists(core_mods) ) {
-		function core_mods() {
-			if ( !is_admin() ) {
-				wp_deregister_script('jquery');
-				wp_register_script('jquery', ("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"), false);
-				wp_enqueue_script('jquery');
-			}
-		}
-		core_mods();
-	}
+// Translations can be filed in the /languages/ directory
+load_theme_textdomain('vburlakFramework', TEMPLATEPATH . '/languages');
 
-	// Clean up the <head>
-	function removeHeadLinks() {
-    	remove_action('wp_head', 'rsd_link');
-    	remove_action('wp_head', 'wlwmanifest_link');
+$locale = get_locale();
+$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+if (is_readable($locale_file))
+    require_once($locale_file);
+
+// Add RSS links to <head> section
+automatic_feed_links();
+
+function register_modernizr_js()
+{
+    wp_enqueue_script('Bootstrap', get_bloginfo('template_directory') . '/_/js/modernizr-1.7.min.js', array(), '1.7', false);
+}
+
+add_action('wp', 'register_modernizr_js');
+
+
+// Load jQuery
+if (!function_exists(core_mods)) {
+    function core_mods()
+    {
+        if (!is_admin()) {
+            wp_deregister_script('jquery');
+            wp_register_script('jquery', ("//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"), false);
+            wp_enqueue_script('jquery');
+        }
     }
-    add_action('init', 'removeHeadLinks');
-    remove_action('wp_head', 'wp_generator');
-    
-    if (function_exists('register_sidebar')) {
-    	register_sidebar(array(
-    		'name' => __('Sidebar Widgets','vburlakFramework' ),
-    		'id'   => 'sidebar-widgets',
-    		'description'   => __( 'These are widgets for the sidebar.'),
-    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    		'after_widget'  => '</div>',
-    		'before_title'  => '<h2>',
-    		'after_title'   => '</h2>'
-    	));
-    }
-    
-    /*Removing wordpress dashboard menus*/
-    /*uncomment what you need to remove*/
-	function remove_menus () {
-	global $menu;
-		$restricted = array(
+
+    core_mods();
+}
+
+// Clean up the <head>
+function removeHeadLinks()
+{
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+}
+
+add_action('init', 'removeHeadLinks');
+remove_action('wp_head', 'wp_generator');
+
+if (function_exists('register_sidebar')) {
+    register_sidebar(array(
+        'name' => __('Sidebar Widgets', 'vburlakFramework'),
+        'id' => 'sidebar-widgets',
+        'description' => __('These are widgets for the sidebar.'),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'
+    ));
+}
+
+/*Removing wordpress dashboard menus*/
+/*uncomment what you need to remove*/
+function remove_menus()
+{
+    global $menu;
+    $restricted = array(
 //				__('Dashboard'), 
 //				__('Posts'), 
 //				__('Media'), 
@@ -58,92 +71,104 @@
 //				__('Settings'), 
 //				__('Comments'), 
 //				__('Plugins')
-			);
-		end ($menu);
-		while (prev($menu)){
-			$value = explode(' ',$menu[key($menu)][0]);
-			if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
-		}
-	}
-	add_action('admin_menu', 'remove_menus');
-    
-    
-    /* Disable WordPress Admin Bar for all users but admins. */
-    show_admin_bar(false);
-    
-    /* Disable WordPress Dashboard Admin Bar Add Button */
-    function removeDashboardAdminBarAddButton() {
-        global $wp_admin_bar;
-        $wp_admin_bar->remove_menu('new-content');
-        $wp_admin_bar->remove_menu('new-post');
-        $wp_admin_bar->remove_menu('new-page');
-        $wp_admin_bar->remove_menu('new-media');
-        $wp_admin_bar->remove_menu('new-link');
-        $wp_admin_bar->remove_menu('new-user');
-        $wp_admin_bar->remove_menu('new-theme');
-        $wp_admin_bar->remove_menu('new-plugin');
+    );
+    end($menu);
+    while (prev($menu)) {
+        $value = explode(' ', $menu[key($menu)][0]);
+        if (in_array($value[0] != NULL ? $value[0] : "", $restricted)) {
+            unset($menu[key($menu)]);
+        }
     }
-    add_action( 'wp_before_admin_bar_render', 'removeDashboardAdminBarAddButton' );
-    
-    /* Removing default Dashboard widgets */
-    function disable_default_dashboard_widgets() {  
-         remove_meta_box('dashboard_right_now', 'dashboard', 'core');  
-         remove_meta_box('dashboard_recent_comments', 'dashboard', 'core');  
-         remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');  
-         remove_meta_box('dashboard_plugins', 'dashboard', 'core');  
-         remove_meta_box('dashboard_quick_press', 'dashboard', 'core');  
-         remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core');  
-         remove_meta_box('dashboard_primary', 'dashboard', 'core');  
-         remove_meta_box('dashboard_secondary', 'dashboard', 'core');  
-    }
-    add_action('admin_menu', 'disable_default_dashboard_widgets');
-    
-    /* Removing comment admin bar button */
-    function removeAdminBarCommentButton() {
-        global $wp_admin_bar;
-        $wp_admin_bar->remove_menu('comments');
-        $wp_admin_bar->remove_menu('updates');
-    }
-    add_action( 'wp_before_admin_bar_render', 'removeAdminBarCommentButton' );
-    
-    add_theme_support( 'post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'audio', 'chat', 'video')); // Add 3.1 post format theme support.
-    
-    add_theme_support( 'post-thumbnails', array( 'post' ) );
+}
 
-    add_theme_support('menus');
-    
-    register_nav_menus(array(  
-			'primary' => 'Главное меню',
-			//'secondary' => 'Второстепенное меню',
-			//'footer' => 'Меню для Подвала',
-		    ));
-    function my_wp_nav_menu_args($args=''){  
-			$args['container'] = '';  
-			return $args;  
-		    }
-    add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
-    
-    // HOVERS for current Post Type in menu
-    
-    //NEED TO CONFIG
-    
-    // WP-eCommerce items - have post-type "wpsc-product"
-    
-    function tg_add_class_to_menu($classes)
-    {
-    	// team is my custom post type
-    	if (is_singular('wpsc-product'))
-    	{
-    		// we're viewing a custom post type, so remove the 'current-page' from all menu items.
-    		$classes = array_filter($classes, "remove_parent");
-    
-    		// add the current page class to a specific menu item.
-    		if (in_array('page-item-23', $classes)) $classes[] = 'current-page-ancestor';
-    	}
-    
-    	return $classes;
+add_action('admin_menu', 'remove_menus');
+
+
+/* Disable WordPress Admin Bar for all users but admins. */
+show_admin_bar(false);
+
+/* Disable WordPress Dashboard Admin Bar Add Button */
+function removeDashboardAdminBarAddButton()
+{
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('new-content');
+    $wp_admin_bar->remove_menu('new-post');
+    $wp_admin_bar->remove_menu('new-page');
+    $wp_admin_bar->remove_menu('new-media');
+    $wp_admin_bar->remove_menu('new-link');
+    $wp_admin_bar->remove_menu('new-user');
+    $wp_admin_bar->remove_menu('new-theme');
+    $wp_admin_bar->remove_menu('new-plugin');
+}
+
+add_action('wp_before_admin_bar_render', 'removeDashboardAdminBarAddButton');
+
+/* Removing default Dashboard widgets */
+function disable_default_dashboard_widgets()
+{
+    remove_meta_box('dashboard_right_now', 'dashboard', 'core');
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'core');
+    remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');
+    remove_meta_box('dashboard_plugins', 'dashboard', 'core');
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'core');
+    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core');
+    remove_meta_box('dashboard_primary', 'dashboard', 'core');
+    remove_meta_box('dashboard_secondary', 'dashboard', 'core');
+}
+
+add_action('admin_menu', 'disable_default_dashboard_widgets');
+
+/* Removing comment admin bar button */
+function removeAdminBarCommentButton()
+{
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('comments');
+    $wp_admin_bar->remove_menu('updates');
+}
+
+add_action('wp_before_admin_bar_render', 'removeAdminBarCommentButton');
+
+add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'audio', 'chat', 'video')); // Add 3.1 post format theme support.
+
+add_theme_support('post-thumbnails', array('post'));
+
+add_theme_support('menus');
+
+register_nav_menus(array(
+    'primary' => 'Главное меню',
+    //'secondary' => 'Второстепенное меню',
+    //'footer' => 'Меню для Подвала',
+));
+function my_wp_nav_menu_args($args = '')
+{
+    $args['container'] = '';
+    return $args;
+}
+
+add_filter('wp_nav_menu_args', 'my_wp_nav_menu_args');
+
+// HOVERS for current Post Type in menu
+
+//NEED TO CONFIG
+
+// WP-eCommerce items - have post-type "wpsc-product"
+
+function tg_add_class_to_menu($classes)
+{
+    // team is my custom post type
+    if (is_singular('wpsc-product')) {
+        // we're viewing a custom post type, so remove the 'current-page' from all menu items.
+        $classes = array_filter($classes, "remove_parent");
+
+        // add the current page class to a specific menu item.
+        if (in_array('page-item-23', $classes)) $classes[] = 'current-page-ancestor';
     }
-    
-    if (!is_admin()) { add_filter('nav_menu_css_class', 'tg_add_class_to_menu'); }
-    
+
+    return $classes;
+}
+
+if (!is_admin()) {
+    add_filter('nav_menu_css_class', 'tg_add_class_to_menu');
+}
+
 ?>
